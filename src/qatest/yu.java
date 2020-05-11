4758b9f183b57728ae30e70529549895ef9855c9
lakers <nirbhay>
2020-05-11 22:56:04 +0530

    my test

---
+++ src/qatest/yu.java
@@ -0,0 +1,11 @@
+package qatest;
+
+public class yu {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=10;i++)
+		// TODO Auto-generated method stub
+System.out.println("Test");
+	}
+
+}
